f805f0b03ac7dd77fc76d6a9eefa7ea43df1a2b7
Roger <morenoroger001@gmail.com>
2021-02-11 00:22:52 -0500

    feat: Add Phateresis to Calculator

--- WPF First Calculator/MainWindow.xaml.cs
+++ WPF First Calculator/MainWindow.xaml.cs
@@ -22,7 +22,8 @@ namespace WPF_First_Calculator
     public partial class MainWindow : Window
     {
         public MainWindow()
-        {            
+        {
+            //MessageBox.Show(Calculator.Calculate("1 + ( 2 * 2 )").ToString());
             InitializeComponent();
             ResizeMode = ResizeMode.CanMinimize;
         }
@@ -35,25 +36,34 @@ namespace WPF_First_Calculator
 
         private void BackspaceButton(object sender, RoutedEventArgs e)
         {
-            if (currentStr.Text.Length == 0)
+            if (currentStr.Text.Length <= 1)
+            { DeleteButton(sender, e); return; }
+
+            if (!Calculator.IsNumber(currentStr.Text.Last().ToString()) || currentStr.Text[currentStr.Text.Length - 2] == ' ')
             {
-                return;
+                currentStr.Text = currentStr.Text.Remove(currentStr.Text.Length - 1);
             }
+
             currentStr.Text =  currentStr.Text.Remove(currentStr.Text.Length - 1);
         }
 
         private void WriteElementInTextBox(object sender, RoutedEventArgs e)
         {
             Button currentButton = (Button)sender;
+            
 
-            if (currentButton.Content.ToString() == "+" || currentButton.Content.ToString() == "-" || 
-                currentButton.Content.ToString() == "*" || currentButton.Content.ToString() == "/" ||
-                currentStr.Text.Length > 0 && Calculator.IsOperator(currentStr.Text.Last().ToString(), new string[] { "+", "-", "/", "*" }))
+            bool isNumber = Calculator.IsNumber(currentButton.Content.ToString());
+            
+
+            if (!isNumber || currentStr.Text.Length > 0 && Calculator.IsOperator(currentStr.Text.Last().ToString(), new string[] { "+", "-", "/", "*", "(", ")" }))                
             {
-                currentStr.Text += " ";                
-            }            
+                currentStr.Text += " ";
+            }
 
             currentStr.Text += currentButton.Content;
+
+            
+
         }
 
         private void EqualsButton(object sender, RoutedEventArgs e)
